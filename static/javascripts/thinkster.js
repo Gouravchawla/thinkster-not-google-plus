2cf09f8bf97e59fce63acc4ee89f6b9533c4da88
Gourav Chawla <gouravchawla@live.com>
2019-03-28 18:54:21 +0400

    feat(auth): Add registration

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,28 @@
-angular
-  .module('thinkster', []);
+(function () {
+  'use strict';
+
+  angular
+      .module('thinkster', [
+          'thinkster.config',
+          'thinkster.routes',
+          'thinkster.authentication'
+      ])
+      .run(run);
+
+  angular
+      .module('thinkster.config', []);
+
+  angular
+      .module('thinkster.routes', ['ngRoute']);
+
+  run.$inject = ['$http'];
+
+  /**
+   * @name run
+   * @desc Update xsrf into $http header to align with Django's default
+   */
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+})();
