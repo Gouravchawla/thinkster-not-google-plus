f13575c352e09e78c825670cb291201e1ede00b8
Gourav Chawla <gouravchawla@live.com>
2019-03-30 09:49:14 +0400

    feat(auth): Add login

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -21,7 +21,12 @@
      * @desc The Factory to be returned
      */
     var Authentication = {
-      register: register
+      getAuthenticatedAccount: getAuthenticatedAccount,
+      isAuthenticated: isAuthenticated,
+      login: login,
+      register: register,
+      setAuthenticatedAccount: setAuthenticatedAccount,
+      unauthenticat: unauthenticate
     };
 
     return Authentication;
@@ -42,7 +47,102 @@
         username: username,
         password: password,
         email: email
-      });
-    }
-  }
+      }).then(registerSuccessFn, registerErrorFn);
+
+      /**
+       * @name registerSuccessFn
+       * @desc Log the new user in
+       */
+      function registerSuccessFn(data, status, headers, config) {
+        Authentication.login(email, password);
+      }
+
+      /**
+       * @name registerErrorFn
+       * @desc Log "Epic failure!" to the console
+       */
+      function registerErrorFn(data, status, headers, config) {
+        console.error("Epic failure");
+      }
+    } // End of register method
+
+    /**
+     * @name login
+     * @desc Try to log in with email `email` and password `password`
+     * @param {string} email The email entered by the user
+     * @param {string} password The password entered by the user
+     * @returns {Promise}
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function login(email, password) {
+      return $http.post('/api/v1/auth/login/', {
+        email: email, password: password
+      }).then(loginSuccessFn, loginErrorFn);
+
+      /**
+       * @name loginSuccessFn
+       * @desc Set the authenticated account and redirect to index
+       */
+      function loginSuccessFn(data, status, headers, config) {
+        Authentication.setAuthenticatedAccount(data.data);
+
+        window.location = '/';
+      } // End of loginSuccessFn method
+
+      /**
+       * @name loginErrorFn
+       * @desc Log "Epic failure!" to the console
+       */
+      function loginErrorFn(data, status, headers, config) {
+        console.error('Epic failure!');
+      } // End of loginErrorFn
+    } // End of login method
+
+    /**
+     * @name getAuthenticatedAccount
+     * @desc Return the currently authenticated account
+     * @returns {object|undefined} Account if authenticated, else `undefined`
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function getAuthenticatedAccount() {
+      if(!$cookies.authenticatedAccount) {
+        return;
+      }
+
+      return JSON.parse($cookies.authenticatedAccount);
+    } // End of getAuthenticatedAccount
+
+    /**
+     * @name isAuthenticated
+     * @desc Check if the current user is authenticated
+     * @returns {boolean} True if user is authenticated, else false
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function isAuthenticated() {
+      // !! would return a boolean value
+      return !!$cookies.authenticatedAccount;
+    } // End of isAuthenticated method
+
+    /**
+     * @name setAuthenticatedAccount
+     * @desc Stringify the account object and store it in a cookie
+     * @param {Object} account The account object to be stored
+     * @returns {undefined}
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function setAuthenticatedAccount(account) {
+      $cookies.authenticatedAccount = JSON.stringify(account);
+    } // End of setAuthenticatedAccount
+
+    /**
+     * @name unauthenticate
+     * @desc Delete the cookie where the user object is stored
+     * @returns {undefined}
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function unauthenticate() {
+      delete $cookies.authenticatedAccount;
+    } // End of unauthenticate
+
+  } // End of Authentication service
 })();
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, patterns, url
 
 from rest_framework_nested import routers
 
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 from thinkster_django_angular_boilerplate.views import IndexView
 
 
@@ -12,6 +12,7 @@ router.register(r'accounts', AccountViewSet)
 urlpatterns = patterns(
     '',
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index')
 
 )
